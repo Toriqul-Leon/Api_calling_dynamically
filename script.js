883ac84b624f1c0036564176f76a6d614abbd171
Toriqul-Leon <yeitoriqul@gmail.com>
2022-02-23 06:04:35 +0600

    DONE

--- script.js
+++ script.js
@@ -8,13 +8,27 @@ loadData();
 const showData = (countries) => {
   const countryContainer = document.getElementById("countries");
   for (const country of countries) {
-    console.log(country.name);
     const div = document.createElement("div");
     countryContainer.appendChild(div);
     div.innerHTML = ` <h2>${country.name.common}</h2>
     <p>${country.capital}</p>
+    <button onclick='loadSingleData("${country.name.common}")'>Show More</button>
 
     `;
     div.classList.add("country");
   }
+};
+
+const loadSingleData = (name) => {
+  const url = `https://restcountries.com/v3.1/name/${name}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCountry(data[0]));
+};
+
+const displayCountry = (country) => {
+  const countryInfo = document.getElementById("country-info");
+  countryInfo.innerHTML = `<h2>${country.name.common}</h2>
+  <img src="${country.flags.png}" >
+  `;
 };
